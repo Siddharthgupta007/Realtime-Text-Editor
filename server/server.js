2b7fcbc84604d3c9f85a5bc7974615ddd3b84365
Siddharth Gupta <siddharth11tha@gmail.com>
2024-03-25 23:16:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,28 +7,29 @@
 
  import { getDocument, updateDocument } from "./controller/document-controller.js";
 
- const PORT = import.meta.env.PORT || 8000;
+ const PORT = 8000;
 
- const URL = import.meta.env.MONGODB_URL || `mongodb+srv://doc:googledoc@googledocsclone.vcdne1y.mongodb.net/?retryWrites=true&w=majority&appName=googleDocsClone`
+ console.log("server is ready");
+
+ const URL =  `mongodb+srv://doc:googledoc@googledocsclone.vcdne1y.mongodb.net/?retryWrites=true&w=majority&appName=googleDocsClone`
  Connection(URL);
 
-  const app = express();
-
+  //const app = express();
 //  if(process.env.NODE_ENV === 'production'){
 //     app.use(express.static('../client/build'));
 //  }
 
- const httpServer = createServer(app);
- httpServer.listen(PORT);
+//  const httpServer = createServer(app);
+//  httpServer.listen(PORT);
 
 
- const io = new Server(httpServer);
-// const io = new Server(PORT, {
-//     cors: {
-//         origin: process.env.FRONT || 'http://localhost:5173',
-//         methods: ['GET', 'POST']
-//     }
-// });
+ //const io = new Server(httpServer);
+const io = new Server(PORT, {
+    cors: {
+        origin: process.env.FRONT || 'http://localhost:5173',
+        methods: ['GET', 'POST']
+    }
+});
 
  io.on('connection', socket => {
      socket.on ('get-document', async documentId =>{
